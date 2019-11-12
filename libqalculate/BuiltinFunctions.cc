b7304fae2838617099d6488044e7eb73bbc34514
Peter Züger <zueger.peter@icloud.com>
2019-11-12 15:08:38 +0100

    factor out unordered_map include mess

--- libqalculate/BuiltinFunctions.cc
+++ libqalculate/BuiltinFunctions.cc
@@ -18,42 +18,12 @@
 #include "Calculator.h"
 #include "Variable.h"
 #include "Unit.h"
+#include "unordered_map.h"
 
 #include <sstream>
 #include <time.h>
 #include <limits>
 #include <algorithm>
-
-#if HAVE_UNORDERED_MAP
-#	include <unordered_map>
-#elif   defined(__GNUC__)
-
-#	ifndef __has_include
-#	define __has_include(x) 0
-#	endif
-
-#	if (defined(__clang__) && __has_include(<tr1/unordered_map>)) || (__GNUC__ >= 4 && __GNUC_MINOR__ >= 3)
-#		include <tr1/unordered_map>
-                namespace Sgi = std;
-#		define unordered_map std::tr1::unordered_map
-#	else
-#		if __GNUC__ < 3
-#			include <hash_map.h>
-                        namespace Sgi { using ::hash_map; }; // inherit globals
-#		else
-#			include <ext/hash_map>
-#			if __GNUC__ == 3 && __GNUC_MINOR__ == 0
-                                namespace Sgi = std;               // GCC 3.0
-#			else
-                                namespace Sgi = ::__gnu_cxx;       // GCC 3.1 and later
-#			endif
-#		endif
-#		define unordered_map Sgi::hash_map
-#	endif
-#else      // ...  there are other compilers, right?
-        namespace Sgi = std;
-#	define unordered_map Sgi::hash_map
-#endif
 
 #define FR_FUNCTION(FUNC)	Number nr(vargs[0].number()); if(!nr.FUNC() || (eo.approximation == APPROXIMATION_EXACT && nr.isApproximate() && !vargs[0].isApproximate()) || (!eo.allow_complex && nr.isComplex() && !vargs[0].number().isComplex()) || (!eo.allow_infinite && nr.includesInfinity() && !vargs[0].number().includesInfinity())) {return 0;} else {mstruct.set(nr); return 1;}
 #define FR_FUNCTION_2(FUNC)	Number nr(vargs[0].number()); if(!nr.FUNC(vargs[1].number()) || (eo.approximation == APPROXIMATION_EXACT && nr.isApproximate() && !vargs[0].isApproximate() && !vargs[1].isApproximate()) || (!eo.allow_complex && nr.isComplex() && !vargs[0].number().isComplex() && !vargs[1].number().isComplex()) || (!eo.allow_infinite && nr.includesInfinity() && !vargs[0].number().includesInfinity() && !vargs[1].number().includesInfinity())) {return 0;} else {mstruct.set(nr); return 1;}
